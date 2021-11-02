cee62407afc77f990f53bb520debb86a35a6bd94
71GM30 <wilmar.vega.mt@correo.usa.edu.co>
2021-11-01 23:53:16 -0500

    first commit

---
+++ src/main/java/com/gruposcrum/grupo8/entities/CategoryCrud.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gruposcrum.grupo8.entities;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author daniel G
+ */
+public interface CategoryCrud extends CrudRepository<Category, Integer>{
+    
+}
